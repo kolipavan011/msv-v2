3fb0d4def9b1b2f840b422b007edcc5f599317af
Pavan Koli <kolipavan011@gmail.com>
2024-02-23 14:18:41 +0530

    Added logo and icon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,9 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-        <div class="container">
-            <a class="navbar-brand" href="/">Mirchi Status Video</a>
+        <div class="container justify-content-md-bewteen justify-content-center">
+            <a class="navbar-brand" href="/">
+                <img height="30px" src="/img/logo.jpg" alt="mirchi status logo">
+            </a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     @foreach ($sidebar['navigation'] as $link)

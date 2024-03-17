e3b7b1bec17c54964ba0ff2dc950c177f7518f97
Pavan Koli <kolipavan011@gmail.com>
2024-03-17 14:49:24 +0530

    fix small log viewer error

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -33,7 +33,7 @@ return [
     |
     */
 
-    'route_path' => config('LOG_VIEWER_ROUTE', 'log-viewer'),
+    'route_path' => env('LOG_VIEWER_ROUTE', 'log-viewer'),
 
     /*
     |--------------------------------------------------------------------------
